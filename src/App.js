a549f1d69464794897adc7a6bae1d32d11e7a58c
Ivan Rubio <smashedpurse@gmail.com>
2023-02-02 11:43:21 -0600

    Import all the components and containers and reflec them into the app file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import { Article,Brand,Cta,Feature,Navbar } from "./components";
-import { Footer } from "./containers";
+import { Brand, Cta, Navbar } from "./components";
+import {Footer,Blog,Features,Header,Possibility,WhatGPT3,} from "./containers";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Brand/>
-    <Cta/>
-    <Feature/>
-    <Article/>
-    <Footer/>
+      <div className="App">
+        <div className="gradient__bg">
+            <Navbar/>
+            <Header/>
+        </div>
+        <Brand/>
+        <WhatGPT3/>
+        <WhatGPT3/>
+        <Features/>
+        <Possibility/>
+        <Cta/>
+        <Blog/>
+        <Footer/>
+      </div>
     </>
   );
 }
 
 export default App;
+
+
