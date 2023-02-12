ff3762b8c3eb498137f06f6596bcc85ac378f768
Ivan Rubio <smashedpurse@gmail.com>
2023-02-11 22:12:33 -0600

    Develope the structure of the blog section

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './Article.css'
+import React from "react";
+import "./Article.css";
 
-const article = () => {
+const article = ({ imgUrl, date, title }) => {
   return (
-    <div>article</div>
-  )
-}
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blogImage" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+};
 
-export default article
+export default article;
